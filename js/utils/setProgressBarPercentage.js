56e9af93a862d4788fda8f6595c55347d0fe2920
Eslam nasser <in1146@fayoum.edu.eg>
2018-08-12 01:08:12 +0200

    :fire: Better Progress Bar

--- js/utils/setProgressBarPercentage.js
+++ js/utils/setProgressBarPercentage.js
@@ -1,9 +1,10 @@
 export default function setProgressBarPercentage(percent) {
-    const ppc = document.querySelector('.progress-pie-chart');
-    const deg = (360 * percent) / 100;
-    if (percent > 50) {
-        ppc.classList.add('gt-50');
+    document.querySelector('.progress-bar-wrapper .bar').style.width = `${percent}%`;
+    document.querySelector('.progress-bar-wrapper .number').innerHTML = `${percent}%`;
+    if (percent === 100) {
+        document.querySelector('.progress-bar-wrapper').style.opacity = 0;
+        setTimeout(() => {
+            document.querySelector('.progress-bar-wrapper').style.display = 'none';
+        }, 500);
     }
-    document.querySelector('.ppc-progress-fill').style.transform = `rotate(${deg}deg)`;
-    document.querySelector('.ppc-percents span').innerHTML = `${percent}%`;
 }
