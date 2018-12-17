9c86fa209987a1a3cf91482c3b6983c5ca056e0c
Eslam nasser <in1146@fayoum.edu.eg>
2018-12-17 03:27:36 +0200

    remove camera from dat gui

--- js/gui/init.js
+++ js/gui/init.js
@@ -8,26 +8,26 @@ export default function initGUI() {
         'Camera Poistion Z': true,
         'Auto rotate camera': false,
     };
-    const CameraPositon = {
-        x: window.camera.position.x,
-        y: window.camera.position.y,
-        z: window.camera.position.z,
-    };
+    // const CameraPositon = {
+    //     x: window.camera.position.x,
+    //     y: window.camera.position.y,
+    //     z: window.camera.position.z,
+    // };
     const gui = new GUI({
         closed: true,
         useLocalStorage: true,
     });
     gui.addFolder('Camera Position');
 
-    gui.add(CameraPositon, 'x', 0, 10).onChange((value) => {
-        window.camera.position.x = value;
-    });
-    gui.add(CameraPositon, 'y', 0, 10).onChange((value) => {
-        window.camera.position.y = value;
-    });
-    gui.add(CameraPositon, 'z', 0, 10).onChange((value) => {
-        window.camera.position.z = value;
-    });
+    // gui.add(CameraPositon, 'x', 0, 10).onChange((value) => {
+    //     window.camera.position.x = value;
+    // });
+    // gui.add(CameraPositon, 'y', 0, 10).onChange((value) => {
+    //     window.camera.position.y = value;
+    // });
+    // gui.add(CameraPositon, 'z', 0, 10).onChange((value) => {
+    //     window.camera.position.z = value;
+    // });
     gui.add(API, 'Auto rotate camera').onChange(() => {
         controls.autoRotateSpeed = API['Auto rotate camera'] ? 0.2 : 0;
     });
--- js/world/controls.js
+++ js/world/controls.js
@@ -3,8 +3,8 @@ import renderer from './renderer';
 
 // CONTROLS
 const controls = new window.THREE.OrbitControls(camera, renderer.domElement);
-// controls.autoRotate = true;
-// controls.autoRotateSpeed = 0;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 0;
 controls.enablePan = true;
 controls.enableDamping = true;
 controls.dampingFactor = 0.25;
