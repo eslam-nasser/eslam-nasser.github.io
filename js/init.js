ffafa9bbba302a7dae6a4de9715a9b7c781409ff
Eslam nasser <in1146@fayoum.edu.eg>
2018-12-18 02:57:58 +0200

    adding penicillium

--- js/init.js
+++ js/init.js
@@ -6,16 +6,32 @@ import onWindowResize from './events/onWindowResize';
 import initGUI from './gui/init';
 import loadControllers from './controllers/loadControllers';
 
-const modelInfo = [
-    {
-        name: 'Spors',
-        url: '../assets/phytophthora-infestans/spors.obj',
-    },
-    {
-        name: 'Hypha',
-        url: '../assets/phytophthora-infestans/hypha.obj',
-    },
-];
+const model1Info = {
+    name: 'Phytophthora infestans',
+    parts: [
+        {
+            name: 'Spors',
+            url: '../assets/phytophthora-infestans/spors.obj',
+        },
+        {
+            name: 'Hypha',
+            url: '../assets/phytophthora-infestans/hypha.obj',
+        },
+    ],
+};
+const model2Info = {
+    name: 'Penicillium',
+    parts: [
+        {
+            name: 'Spors',
+            url: '../assets/penicillium/spors.obj',
+        },
+        {
+            name: 'Hypha',
+            url: '../assets/penicillium/hypha.obj',
+        },
+    ],
+};
 
 // TODO: MAKE ARROWS DYNAMIC
 function makeArrow(x, y, z) {
@@ -41,10 +57,37 @@ export default function init() {
     scene.add(floor);
 
     // Load Model
-    loadModels(modelInfo, scene);
+    loadModels(model1Info.parts, scene);
 
     // Load controllers
-    loadControllers(modelInfo);
+    loadControllers(model1Info.parts);
+
+    // Load model name
+
+    // TEXT
+    const loader = new window.THREE.FontLoader();
+
+    loader.load('../assets/fonts/helvetiker_regular.typeface.json', (font) => {
+        const geometry = new window.THREE.TextGeometry(model1Info.name.split(' ').join('\n'), {
+            font,
+            size: 1 / 2.2,
+            height: 0.01,
+        });
+        const materials = [
+            new window.THREE.MeshBasicMaterial({
+                color: 0x2206669,
+            }),
+            new window.THREE.MeshBasicMaterial({ color: 0x2206669 }),
+        ];
+        const name = new window.THREE.Mesh(geometry, materials);
+        name.rotation.x = Math.PI / -2;
+        name.position.x = -1.85;
+        name.position.y = 0;
+        name.position.z = -4.7;
+        window.f_name = name;
+
+        scene.add(name);
+    });
 
     // Arrows
     // const ar = makeArrow(1, 2, 0);
--- js/world/scene.js
+++ js/world/scene.js
@@ -12,32 +12,4 @@ const backgroundSphere = new window.THREE.Mesh(
 );
 scene.add(backgroundSphere);
 
-// TEXT
-const loader = new window.THREE.FontLoader();
-
-loader.load('../assets/fonts/helvetiker_regular.typeface.json', (font) => {
-    const geometry = new window.THREE.TextGeometry('Phytophthora \ninfestans', {
-        font,
-        size: 1 / 2.2,
-        height: 0.01,
-    });
-    // geometry.computeBoundingBox();
-    // const centerOffset = -0.5 * (geometry.boundingBox.max.x - geometry.boundingBox.min.x);
-
-    const materials = [
-        new window.THREE.MeshBasicMaterial({
-            color: 0x2206669,
-        }),
-        new window.THREE.MeshBasicMaterial({ color: 0x2206669 }),
-    ];
-    const name = new window.THREE.Mesh(geometry, materials);
-    name.rotation.x = Math.PI / -2;
-    name.position.x = -1.85;
-    name.position.y = 0;
-    name.position.z = -4.7;
-    window.f_name = name;
-
-    scene.add(name);
-});
-
 export default scene;
