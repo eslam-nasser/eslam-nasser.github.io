193908d71e0d360600a074662c2daf31ee661acb
Eslam nasser <in1146@fayoum.edu.eg>
2019-01-15 01:20:50 +0200

    Add colors controls

--- js/gui/init.js
+++ js/gui/init.js
@@ -1,13 +1,32 @@
+/* eslint no-param-reassign: ["error", { "props": false }] */
+
 import { GUI } from '../libs/dat.gui.min';
 import controls from '../world/controls';
+import scene from '../world/scene';
+import hexToRGB from '../utils/hexToRGB';
 
-export default function initGUI() {
+export default function initGUI(modelParts) {
     const API = {
         'Camera Poistion X': true,
         'Camera Poistion Y': true,
         'Camera Poistion Z': true,
         'Auto rotate camera': false,
     };
+
+    // scene.traverse((node) => {
+    // console.log(node);
+    // if (node.treeNode && node.treeNode.sid) {
+    // }
+    // sid.forEach(function (id) {
+
+    //         removeNode(node, scene);
+
+    //     }
+    // });
+    // });
+    // const colors = {
+    //     spors: '#000',
+    // };
     // const CameraPositon = {
     //     x: window.camera.position.x,
     //     y: window.camera.position.y,
@@ -27,6 +46,25 @@ export default function initGUI() {
     // gui.add(CameraPositon, 'z', 0, 10).onChange((value) => {
     //     window.camera.position.z = value;
     // });
+
+    const colors = {};
+    modelParts.forEach(({ name, color }) => {
+        colors[name] = color || '#000';
+    });
+
+    Object.keys(colors).forEach((key) => {
+        gui.addColor(colors, key).onChange((e) => {
+            const object = scene.getObjectByName(key, true);
+            object.traverse((node) => {
+                if (node instanceof window.THREE.Mesh) {
+                    const { r, g, b } = hexToRGB(e);
+                    node.material.color.setRGB(r / 255, g / 255, b / 255);
+                }
+            });
+        });
+    });
+    // console.log(name, object);
+
     gui.add(API, 'Auto rotate camera').onChange(() => {
         controls.autoRotateSpeed = API['Auto rotate camera'] ? 0.2 : 0;
     });
